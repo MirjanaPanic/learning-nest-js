7f4c6cd7d570c04f0ac0a1a2917754afd44c3d14
MirjanaPanic <panicmirjanaa@gmail.com>
2025-07-22 20:26:33 +0200

    post and get

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
+//zahtevi koji dolaze na /products
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  //injecting the service
+  constructor(private productsService: ProductsService) {}
+
+  @Post()
+  addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    const generatedId = this.productsService.insertProduct(
+      prodTitle,
+      prodDesc,
+      prodPrice,
+    );
+    return { id: generatedId };
+  }
+
+  @Get('all') //products/all
+  getAllProducts() {
+    return this.productsService.getAllProducts();
+  }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './products.model';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  private products: Product[] = [];
+
+  insertProduct(title: string, description: string, price: number) {
+    const prodId = new Date().toString();
+    this.products.push(new Product(prodId, title, description, price));
+    return prodId;
+  }
+
+  getAllProducts() {
+    return this.products;
+  }
+}
